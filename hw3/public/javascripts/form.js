b8f36020ce3ec2ca52830c5e17faa5e0e323c36f
barkerj26 <barkerj26@up.edu>
2024-02-07 18:42:07 -0800

    finalize for hw3

--- hw3/public/javascripts/form.js
+++ hw3/public/javascripts/form.js
@@ -1,3 +1,8 @@
+/*
+ *	Made by Jaden Barker for CS-341
+ */
+
+// handles submitting the cheesecake form
 submitHandler = function(event) {
 	if ($("#notes").val().toLocaleLowerCase().includes("vegan")) {
 		alert("Cheesecakes contain dairy, they are not vegan.");
@@ -7,10 +12,12 @@ submitHandler = function(event) {
 	}
 }
 
+// handles selecting month for cheesecake order stats
 monthHandler = function(event) {
 	$("#month").text($(this).text());
 }
 
+// apply handlers to appropriate buttons
 $(function() {
 	$("#submit").click(submitHandler);
 	$(".dropdown-button").click(monthHandler);
---
+++ hw3/tests/cool.test.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+test('test selectEvent', () => {
+	// read the index.html file into a string
+	var html = fs.readFileSync('public/index.html', 'utf8');
+	expect(html).toEqual(expect.anything());
+
+	// put the html into a testing dom and do a sanity check
+	document.body.innerHTML = html;
+	const $ = require('jquery');
+	expect($('h1').html()).toBe("Cheesecake Order Form");
+});
